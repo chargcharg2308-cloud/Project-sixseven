895d43f67da49878ac0a29d101a926231c29abaf
sayada06 <sayadasyd060650@gmail.com>
2026-07-18 14:44:34 +0700

    UPDATE : add_member.py & remove_member.py

--- personnel/remove_member.py
+++ personnel/remove_member.py
@@ -1,5 +1,5 @@
 # =====================================================
-#  personnel/remove_member.py — คนรับผิดชอบ: ______________________
+#  personnel/remove_member.py — คนรับผิดชอบ: โฟม
 #  หน้าที่: ลบลูกน้องออกจากแฟมิลี่ตามชื่อ
 # =====================================================
 from data import family_members
@@ -8,11 +8,16 @@ def remove_member(target_name):
 #   - หาคนที่ชื่อตรงกับ target_name (ไม่สนตัวพิมพ์ใหญ่/เล็ก) แล้วลบออกจาก family_members
 #   - ลบสำเร็จ -> return True | ไม่เจอ -> return False
     # TODO: เขียนโค้ดตรงนี้
-    pass
+    family_members = input("Enter remove name : ")
+    for person_dict in family_members:
+        if person_dict["name"] == target_name:
+            family_members.remove(person_dict)
+            return True
+    return False
 
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m personnel.remove_member
-if __name__ == "__main__":
-    print(remove_member("Luigi"))   # ครั้งแรกต้องได้ True
-    print(remove_member("Luigi"))   # ครั้งที่สองต้องได้ False (ลบไปแล้ว)
-    print(family_members)           # ต้องเหลือแค่ Tony
+# if __name__ == "__main__":
+#     print(remove_member("Luigi"))   # ครั้งแรกต้องได้ True
+#     print(remove_member("Luigi"))   # ครั้งที่สองต้องได้ False (ลบไปแล้ว)
+#     print(family_members)           # ต้องเหลือแค่ Tony
