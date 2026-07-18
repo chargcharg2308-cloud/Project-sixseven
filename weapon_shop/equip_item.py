9eb2fe819732e58d01cdfd5940931c6547453303
Student <Student@Train11-4-PC061.control.ad.sit.kmutt.ac.th>
2026-07-18 16:00:41 +0700

    weapon

--- weapon_shop/equip_item.py
+++ weapon_shop/equip_item.py
@@ -98,10 +98,10 @@ def equip_item(person, weapon):
 
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m weapon_shop.equip_item
-if __name__ == "__main__":
-    vito = {"name": "Vito", "money": 60000, "power": 5, "equipment": "ไม่มี"}
-    gun = {"name": "ปืนพก 9mm", "price": 50000, "bonus": 5}
+# if __name__ == "__main__":
+#     vito = {"name": "Vito", "money": 60000, "power": 5, "equipment": "ไม่มี"}
+#     gun = {"name": "ปืนพก 9mm", "price": 50000, "bonus": 5}
 
-    print(equip_item(vito, gun))   # ต้องได้ status True
-    print(vito)                    # เงินเหลือ 10000, power เป็น 10, equipment เป็นปืน
-    print(equip_item(vito, gun))   # ครั้งที่สองต้องได้ "มีอาวุธอยู่แล้ว"
+#     print(equip_item(vito, gun))   # ต้องได้ status True
+#     print(vito)                    # เงินเหลือ 10000, power เป็น 10, equipment เป็นปืน
+#     print(equip_item(vito, gun))   # ครั้งที่สองต้องได้ "มีอาวุธอยู่แล้ว"
